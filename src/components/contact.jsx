477876058cbd09db3599d5f6c246d7fb7c94b320
Ritesh Patil <105771789+Ritzz09@users.noreply.github.com>
2025-02-13 15:04:51 +0530

    Update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -29,7 +29,7 @@ export const Contact = (props) => {
     setLoading(true);
   
     try {
-      const response = await fetch("http://localhost:5000/send-email", {
+      const response = await fetch("http://localhost:5001/send-email", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
