e03eb528a9053253c04ca174b44ad9f8b1af7535
Ritesh Patil <105771789+Ritzz09@users.noreply.github.com>
2025-02-13 15:17:37 +0530

    Update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -29,7 +29,7 @@ export const Contact = (props) => {
     setLoading(true);
   
     try {
-      const response = await fetch("http://localhost:5001/send-email", {
+      const response = await fetch("https://sympony.vercel.app", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
